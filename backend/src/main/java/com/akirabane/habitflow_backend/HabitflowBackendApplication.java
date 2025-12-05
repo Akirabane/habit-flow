bb8f8df41dd8c1d2c3f3d97d2602ac948d5d1b2f
joshua benon <joshuabenon988@yahoo.fr>
2025-12-06 00:26:58 +0100

    initial Spring Boot project
    Initialized base backend setup for springboot project using Spring Initializr.

---
+++ backend/src/main/java/com/akirabane/habitflow_backend/HabitflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akirabane.habitflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitflowBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/akirabane/habitflow_backend/HabitflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akirabane.habitflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
